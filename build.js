8ebf726b1a0ab2aeb19a219de9ccdce80fe10ec6
Eric Wang <eric.wang04@sap.com>
2025-08-31 12:15:32 +0800

    add build

--- build.js
+++ build.js
@@ -5,6 +5,33 @@ const src = path.join(__dirname, "index.html");
 const destDir = path.join(__dirname, "dist");
 const dest = path.join(destDir, "index.html");
 
+// write date time to html content
+const dateTime = new Date().toLocaleString();
+console.log(`Building at ${dateTime}`);
+
+const htmlContent = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Empty Page</title>
+    <script>
+      document.addEventListener("DOMContentLoaded", function () {
+        document.querySelector(
+          "h1"
+        ).textContent = 'Hello, World! ${dateTime}';
+      });
+    </script>
+  </head>
+  <body>
+    <h1></h1>
+  </body>
+</html>
+`;
+
+// Write the HTML content to index.html
+fs.writeFileSync(src, htmlContent);
+
 // Ensure dist directory exists
 if (!fs.existsSync(destDir)) {
   fs.mkdirSync(destDir, { recursive: true });
